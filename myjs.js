c82da00a01c87c3e5a3d6fe2911788f987b8116a
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-05-08 13:17:18 -0400

    yes

--- myjs.js
+++ myjs.js
@@ -1,16 +1,18 @@
-function fibonacci(num){
-    var a = 1, b = 0, temp;
-  
-    while (num >= 0){
-      temp = a;
-      a = a + b;
-      b = temp;
-      num--;
+function fibonacci(num) {
+    var a = 1,
+        b = 0,
+        temp;
+
+    while (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
     }
-  
+
     return b;
-  }
-  
+}
+
 window.onload = function() {
     //get a reference to the element
     var myBtn = document.getElementById('compfib');
@@ -20,6 +22,7 @@ window.onload = function() {
         var myform = document.getElementById('fibnum');
         var result = document.getElementById('fibans');
         var answer = fibonacci(parseInt(myform.value));
-        result.innerHTML = "Answer: "+answer.toString();
+        pusg
+        result.innerHTML = "Answer: " + answer.toString();
     });
 }
